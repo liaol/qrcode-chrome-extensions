2226d297fd5278383d2020a4cca85d0e9a2a483f
liaol <liaol@qiu.com>
2015-01-13 20:23:34 +0800

    add text

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,9 @@
 +function( document, $ ) {
-    var qrcode = document.getElementById('qrcode'),
-    url = document.getElementById('url');
+    var qrcode = document.getElementById('qrcode');
+    var url = document.getElementById('url');
     chrome.tabs.getSelected(function( tab ){
             $(qrcode).qrcode({width: 150,height: 150,text: tab.url});
+            url.value = tab.url;
+            url.select();
     });
 }( document, jQuery ); 
